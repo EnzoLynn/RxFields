470040ae3c9cfab3f0f4ebb7b8a5a9e655055e0b
chenheling <6710582@qq.com>
2017-05-09 12:31:19 +0800

    rx

--- src/weui/RxFields.js
+++ src/weui/RxFields.js
@@ -1,9 +1,11 @@
 import CheckboxGroup from './CheckboxGroup'
 import RadioGroup from './RadioGroup'
-import renderField from './renderField' 
+import renderField from './renderField'
+import renderRcSelect from './renderRcSelect'
 
 export {
 	CheckboxGroup,
 	RadioGroup,
-	renderField 
+	renderField,
+	renderRcSelect
 }
